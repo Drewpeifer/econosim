943b8a9b927d652545b053f88aae1afaa0eb4041
drewpeifer <peiftronica@gmail.com>
2021-04-16 06:00:53 -0400

    downgraded to vue2 b/c vue3 is a big pile of nonsense

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import $ from 'jquery'
-
-createApp(App).mount('#app')
-
+//////////////
+// Page load, counter start, BGS binding
 class Timer {
 	// shamlessly ripped from https://stackoverflow.com/a/57981688/2302770
 	constructor () {
@@ -16,7 +12,6 @@ class Timer {
 		if (!this.startTime) {
 			return 0;
 		}
-
 		return Date.now() - this.startTime;
 	}
 
@@ -24,9 +19,7 @@ class Timer {
 		if (this.isRunning) {
 			return console.error('Timer is already running');
 		}
-
 		this.isRunning = true;
-
 		this.startTime = Date.now();
 	}
 
@@ -34,20 +27,16 @@ class Timer {
 		if (!this.isRunning) {
 			return console.error('Timer is already stopped');
 		}
-
 		this.isRunning = false;
-
 		this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
 	}
 
 	reset () {
 		this.overallTime = 0;
-
 		if (this.isRunning) {
 			this.startTime = Date.now();
 			return;
 		}
-
 		this.startTime = 0;
 	}
 
@@ -61,35 +50,41 @@ class Timer {
 		return this.overallTime;
 	}
 }
-
-function getRandomInt(min, max) {
-	// inclusive
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//////////////
-// Page load, counter start, BGS binding
 $(function() {
-	$('#pauseTimer').bind('click', function() {
-		let $this = $(this);
+		var timer = new Timer();
+		timer.start();
+		setInterval(() => {
+			const timeInSeconds = Math.round(timer.getTime() / 1000);
+			document.getElementById('time').innerText = timeInSeconds;
+		}, 100);
+		$('#pauseTimer').bind('click', function() {
+			let $this = $(this);
 
-		if ($this.hasClass('paused')) {
-			timer.start();
-			$this.removeClass('paused');
-		} else {
-			timer.stop();
-			$this.addClass('paused');
-		}
+			if ($this.hasClass('paused')) {
+				timer.start();
+				$this.removeClass('paused');
+			} else {
+				timer.stop();
+				$this.addClass('paused');
+			}
+		});
+		$('#resetTimer').bind('click', function() {
+			timer.reset();
+		});
+		setInterval(() => {
+			if (timer.isRunning) {
+				app.counter++;
+			}
+		}, 3000);
 	});
-	$('#resetTimer').bind('click', function() {
-		timer.reset();
-	});
-	const timer = new Timer();
-	timer.start();
-	setInterval(() => {
-		const timeInSeconds = Math.round(timer.getTime() / 1000);
-		document.getElementById('time').innerText = timeInSeconds;
-	}, 100)
+
+// main Vue app, wraps market component
+var app = new Vue({
+		el: '#app',
+		data: {
+			counter : 1
+		},
+        methods: {
+
+        }
 });
